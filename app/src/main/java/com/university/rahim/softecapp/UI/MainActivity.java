7c80c894e7d15767377adc4d4d98dcde404dbc67
Algorate <algorate@gmail.com>
2018-03-10 14:03:59 +0500

    firebase-auth-incomplete

--- app/src/main/java/com/university/rahim/softecapp/UI/MainActivity.java
+++ app/src/main/java/com/university/rahim/softecapp/UI/MainActivity.java
@@ -1,15 +1,88 @@
 package com.university.rahim.softecapp.UI;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.university.rahim.softecapp.R;
 
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    public static final int RC_SIGN_IN = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener()
+        {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth)
+            {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user!=null)
+                {
+                    //signed in
+                    //user.getEmail()
+
+                    //Toast.makeText(MainActivity.this, "This is my Toast message!",
+                    //        Toast.LENGTH_LONG).show();
+                    Log.d("saad","logged in");
+
+                }
+                else
+                {
+
+                    Log.d("saad","logged out");
+
+                    //signed out
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setIsSmartLockEnabled(false)
+
+                                    .setAvailableProviders(Arrays.asList(
+          //                                  new AuthUI.IdpConfig.EmailBuilder().build(),
+                                            new AuthUI.IdpConfig.GoogleBuilder().build()
+                                            ))
+                                    .build(),
+                            RC_SIGN_IN);
+                }
+            }
+        };
+
     }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+    }
+
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+
+    }
+
 }
